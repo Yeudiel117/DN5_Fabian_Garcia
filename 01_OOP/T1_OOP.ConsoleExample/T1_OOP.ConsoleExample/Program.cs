d3ec6aa3ceeded1f196601319ce7bb5fef889a93
Fabian Garcia <fabian.yeudiel117@gmail.com>
2022-12-04 19:35:02 -0600

    Tutoriales terminados

--- 01_OOP/T1_OOP.ConsoleExample/T1_OOP.ConsoleExample/Program.cs
+++ 01_OOP/T1_OOP.ConsoleExample/T1_OOP.ConsoleExample/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AudioManager;
 
 namespace T1_OOP.ConsoleExample
 {
@@ -10,6 +11,12 @@ namespace T1_OOP.ConsoleExample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+
+            Animal animal= new Animal();
+            animal.AnimalSound();
+
+            Console.ReadKey();
         }
     }
 }
