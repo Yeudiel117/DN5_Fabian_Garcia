4d9b9cd17583c1c424c857a60cb75208d1a9c7dc
Fabian Garcia <fabian.yeudiel117@gmail.com>
2022-12-09 18:13:44 -0600

    E1 - E2 terminados

---
+++ 02_WebDevelopment/E2/GymManager.Web/GymManager.Web/Models/Member.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GymManager.Web.Models
+{
+    public class Member
+    {
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDay { get; set; }
+
+        public int CintityId { get; set; }
+
+        public string Email { get; set; }
+
+        public bool AlowNewsletter { get; set; }
+
+    }
+}
