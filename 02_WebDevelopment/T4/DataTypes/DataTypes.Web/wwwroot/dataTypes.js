fde27877d73286bf7f80a53b5f5cf693bd138f1e
Fabian Garcia <fabian.yeudiel117@gmail.com>
2022-12-12 13:38:17 -0600

    T4 terminado

---
+++ 02_WebDevelopment/T4/DataTypes/DataTypes.Web/wwwroot/dataTypes.js
@@ -0,0 +1,28 @@
+﻿
+var element = document.getElementById("example");
+var textExample = "this is a text";
+var aNumber = 10.3333;
+
+var complexObject = {
+    name: "Israel",
+    lastname: "Perez",
+    age: 35
+};
+
+/*
+var text1 = "Text";
+var text2 = 'Text';
+
+var ul = "<ul>";
+
+
+for (var i = 0; i < textExample.length; i++) {
+    ul += ("<li>" + textExample[i] + "</li>");
+}
+
+ul += "</ul>";
+*/
+
+var data = "133333";
+
+element.innerHTML = typeof (parseInt(data));
