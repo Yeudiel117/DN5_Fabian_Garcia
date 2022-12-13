60617a79bd6625a3983055a80745743691ec5e0a
Fabian Garcia <fabian.yeudiel117@gmail.com>
2022-12-12 18:10:05 -0600

    E3 terminado

---
+++ 02_WebDevelopment/E3/DataTypes/DataTypes.Web/wwwroot/dataTypes.js
@@ -0,0 +1,42 @@
+﻿
+function onButtonClick(evt) {
+    ///////////////////////////
+    var t = document.getElementById("sampleUl");
+
+    var currentHtml = t.innerHTML;
+
+    for (var i = 0; i < arrayCustomers.length; i++) {
+
+        t.innerHTML += "<li>" + "Name: " + arrayCustomers[i].name + "Registration Date: " + arrayCustomers[i].date + "</li>";
+
+    }
+    //////////////////////////
+}
+
+var button = document.getElementById("arrayButton");
+
+button.addEventListener("click", onButtonClick);
+
+var date = new Date();
+
+class Customers {
+    constructor(name, date) {
+        this.name = name;
+        this.date = date;
+    }
+}
+
+var arrayCustomers = [
+    new Customers("Juancho ", date.toUTCString()),
+    new Customers("Miguel ", date.toUTCString()),
+    new Customers("Roberto ", date.toUTCString()),
+    new Customers("Jason ", date.toUTCString()),
+    new Customers("Maria ", date.toUTCString()),
+    new Customers("Daniela ", date.toUTCString()),
+    new Customers("Ramirez ", date.toUTCString()),
+    new Customers("Jose ", date.toUTCString()),
+    new Customers("Alberto ", date.toUTCString()),
+    new Customers("Fernando ", date.toUTCString())
+];
+
+
---
+++ 02_WebDevelopment/E3/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer() { Name = "Juancho", RegistrationDate = DateTime.Now},
+                new Customer() { Name = "Miguel", RegistrationDate = DateTime.Now},
+                new Customer() { Name = "Roberto", RegistrationDate = DateTime.Now},
+                new Customer() { Name = "Jason", RegistrationDate = DateTime.Now},
+                new Customer() { Name = "Maria", RegistrationDate = DateTime.Now},
+                new Customer() { Name = "Daniela", RegistrationDate = DateTime.Now},
+                new Customer() { Name = "Ramirez", RegistrationDate = DateTime.Now},
+                new Customer() { Name = "Jose", RegistrationDate = DateTime.Now},
+                new Customer() { Name = "Alberto", RegistrationDate = DateTime.Now},
+                new Customer() { Name = "Fernando", RegistrationDate = DateTime.Now}
+            };
+
+            Console.WriteLine("Bucle foreach");
+            foreach (var customer in customers)
+            {
+                Console.WriteLine($"{customers.IndexOf(customer)+1}.- Name: {customer.Name} Registration Date: {customer.RegistrationDate}");
+            }
+
+            Console.WriteLine("\nBucle For");
+            for (int i = 0; i < customers.Count; i++)
+            {
+                Console.WriteLine($"{i+1}.- Name: {customers[i].Name} Registration Date: {customers[i].RegistrationDate}");
+            }
+
+            Console.WriteLine("\nBucle while");
+            var aux = 0;
+            while (aux < customers.Count)
+            {
+                Console.WriteLine($"{aux+1}.- Name: {customers[aux].Name} Registration Date: {customers[aux].RegistrationDate}");
+                aux++;
+            }
+
+            Console.WriteLine("\nBucle do while");
+            aux = 0;
+            do
+            {
+                Console.WriteLine($"{aux+1}.- Name: {customers[aux].Name} Registration Date: {customers[aux].RegistrationDate}");
+                aux++;
+            } while (aux < customers.Count);
+        }
+    }
+}
