8f57f162a4ee3e4d93d9ad0d89a16d7115477f4c
Fabian Garcia <fabian.yeudiel117@gmail.com>
2022-12-14 17:40:41 -0600

    E4 terminado

---
+++ 02_WebDevelopment/E4/GymManager.Web/GymManager.Web/Controllers/AttendanceController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymManager.Web.Controllers
+{
+    public class AttendanceController : Controller
+    {
+
+        public IActionResult MemberIn()
+        {
+            return View();
+        }
+
+        public IActionResult MemberOut()
+        {
+            return View();
+        }
+    }
+}
