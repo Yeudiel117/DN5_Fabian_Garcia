61285d0f9a96ce604e12ebf154b5a3048172cdfa
Fabian Garcia <fabian.yeudiel117@gmail.com>
2023-01-04 14:56:02 -0600

    03_T3 terminado

---
+++ 03_NetProjects/T3/GymManager.Web/GymManager.Web/Controllers/MembersController.cs
@@ -0,0 +1,51 @@
+﻿using GymManager.ApplicationServices.Members;
+using GymManager.Core.Members;
+using GymManager.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GymManager.Web.Controllers
+{
+    public class MembersController : Controller
+    {
+
+        private readonly IMembersAppService _membersAppService;
+
+        public MembersController(IMembersAppService membersAppService) 
+        {
+            _membersAppService = membersAppService;
+        }
+
+        public IActionResult Index()
+        {
+
+            List<Member> members = _membersAppService.GetMembers();
+
+            MemberListViewModel viewModel= new MemberListViewModel();
+
+            viewModel.NewMembersCount = 2;
+            viewModel.Members = members;
+
+            return View(viewModel);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Member member)
+        {
+
+            _membersAppService.AddMember(member);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
