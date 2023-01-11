6bf3ac30b669ac2e9676193f102aa42d2cdaba63
Fabian Garcia <fabian.yeudiel117@gmail.com>
2023-01-11 15:00:27 -0600

    03_T7 terminado

--- 03_NetProjects/T7/GymManager.Web/GymManager.Web/Controllers/AccountController.cs
+++ 03_NetProjects/T7/GymManager.Web/GymManager.Web/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using GymManager.Web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace GymManager.Web.Controllers
@@ -10,11 +11,18 @@ namespace GymManager.Web.Controllers
     {
 
         private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
         public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager) 
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
 
+            if (!_userManager.Users.Any())
+            {
+                var result = _userManager.CreateAsync(new IdentityUser { Email = "rodo@ejemplo.com", UserName = "rodo@ejemplo.com",
+                    EmailConfirmed = true, }, "admin123").Result;
+            }
         }
 
         public IActionResult Login()
@@ -25,7 +33,26 @@ namespace GymManager.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginModel model)
         {
+            string returnUrl = string.IsNullOrEmpty(Request.Query["returnUrl"]) ? Url.Content("~/") : Request.Query["returnUrl"];
 
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                if(result.Succeeded)
+                {
+                    return LocalRedirect(returnUrl);
+                }
+
+                if(result.IsLockedOut)
+                {
+                    return RedirectToPage("./Lockout");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt");
+                    return View();
+                }
+            }
 
             return View();
         }
