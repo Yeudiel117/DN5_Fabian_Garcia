1826161c0c0e012f316ab6030e0d09116dd2de96
Fabian Garcia <fabian.yeudiel117@gmail.com>
2023-01-11 14:10:21 -0600

    03_T7 avance

---
+++ 03_NetProjects/T7/GymManager.Web/GymManager.Web/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using GymManager.Core.Members;
+using GymManager.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GymManager.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager) 
+        {
+
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ 03_NetProjects/T7/GymManager.Web/GymManager.Web/Models/MenuViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GymManager.Core.Navigation;
+using System.Collections.Generic;
+
+namespace GymManager.Web.Models
+{
+    public class MenuViewModel
+    {
+        public string CurrentPageName { get; set; }
+
+        public List<UserMenuItem> Menu { get; set; }
+    }
+}
